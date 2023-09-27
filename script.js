7cb42ef08a72317e17ef37d141a27d49d8a274d6
Sarah <sarahwambui843@gmail.com>
2023-09-27 23:19:44 +0300

    :sparkles: feat: added a new paragraph through javascript

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ button.addEventListener("click", handleSubmit)
 function handleSubmit(e){
     alert("I was clicked!")
 }
+
+let p = document.createElement("p")
+p.textContent= "I was created through JavaScript"
+document.querySelector("body").appendChild(p)
